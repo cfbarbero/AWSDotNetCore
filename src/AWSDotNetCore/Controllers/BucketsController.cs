7b2ad00e3391149658e476beab5560d9b4c4f30d
crisb <cfbarbero@gmail.com>
2017-01-06 13:07:30 -0700

    initial commit

---
+++ src/AWSDotNetCore/Controllers/BucketsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.S3;
+
+namespace AWSDotNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class BucketsController : Controller
+    {
+        private IAmazonS3 S3Client;
+
+        public BucketsController(IAmazonS3 s3Client)
+        {
+            this.S3Client = s3Client;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var buckets = S3Client.ListBucketsAsync();
+
+            return buckets.Result.Buckets.Select(x => x.BucketName);
+        }
+
+    }
+}
